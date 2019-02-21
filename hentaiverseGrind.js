67025539b3126b364e9059e3683407bda94ed279
arconom <arconom@hotmail.com>
2019-02-21 11:58:55 -0800

    Update hentaiverseGrind.js

--- hentaiverseGrind.js
+++ hentaiverseGrind.js
@@ -1,15 +1,12 @@
 NodeList.prototype.forEach = Array.prototype.forEach;
 
-function loop()
-{
-	return setInterval(function ()
-	{
+function loop() {
+	return setInterval(function () {
 		chooseAction()();
 	}, 1000);
 }
 
-function chooseAction()
-{
+function chooseAction() {
 	var prompt = document.querySelector("#btcp");
 	var health = 0;
 	var mana = parseInt(document.querySelector("#pane_vitals #vrm").textContent);
@@ -17,15 +14,13 @@ function chooseAction()
 	var tokenCount = document.querySelectorAll("#vcp > div > div").length;
 	var healAvailable = document.querySelector("#qb1[onclick]") == null ? false : true;
 
-	try{
+	try {
 		health = parseInt(document.querySelector("#pane_vitals #vrhb").textContent);
-	}
-	catch(e){
+	} catch (e) {
 		health = 0;
 	}
 
-	var mapping =
-	{
+	var mapping = {
 		"attack": 0,
 		"defend": 0,
 		"focus": 0,
@@ -35,90 +30,72 @@ function chooseAction()
 		"fire": 0
 	};
 
-	if (!prompt)
-	{
-		if ((mana < (130 * .8)) && (tokenCount > 0))
-		{
-			mapping["focus"] += 1;
+	if (!prompt) {
+		if ((mana < (130 * 0.8)) && (tokenCount > 0)) {
+			mapping.focus += 1;
 		}
-		if ((health < ((950 * 0.7))) && (tokenCount > 0))
-		{
-			mapping["defend"] += 1;
+		if ((health < ((950 * 0.7))) && (tokenCount > 0)) {
+			mapping.defend += 1;
 		}
-		if ((mana > 8) && (health < ((950 * .33))) && (healAvailable))
-		{
-			mapping["heal"] += 5;
+		if ((mana > 8) && (health < ((950 * 0.33))) && (healAvailable)) {
+			mapping.heal += 5;
 		}
-		if (monsterCount > 1)
-		{
-			mapping["attack"] += 1;
+		if (monsterCount > 1) {
+			mapping.attack += 1;
 		}
-		if ((groupedMonsters() !== -1) && (mana > 2))
-		{
-			mapping["fire"] += 3;
+		if ((groupedMonsters() !== -1) && (mana > 2)) {
+			mapping.fire += 3;
 		}
 
-		mapping["attack"] += 1;
+		mapping.attack += 1;
 
-		return getFunction(Object.keys(mapping).reduce(function (a, b)
-			{
-				return mapping[a] > mapping[b] ? a : b
-			}
-			));
-	}
-	else
-	{
+		var returnMe = getFunction(Object.keys(mapping).reduce(function (a, b) {
+					return mapping[a] > mapping[b] ? a : b
+				}));
+
+		return returnMe;
+	} else {
 		prompt.click();
 	}
 
 }
-function groupedMonsters(){
-	
+function groupedMonsters() {
+
 	var monsters = document.querySelectorAll("#pane_monster > div");
 	var i;
-	
-	for(i = 1; i < monsters.length - 1; i++){
-		if((monsters[i - 1].onclick !== null) 
-			&& (monsters[i].onclick !== null)
-			&& (monsters[i + 1].onclick !== null)){
+
+	for (i = 1; i < monsters.length - 1; i++) {
+		if ((monsters[i - 1].onclick !== null)
+			 && (monsters[i].onclick !== null)
+			 && (monsters[i + 1].onclick !== null)) {
 			return i;
 		}
 	}
 	return -1;
 }
-function getFunction(value)
-{
-	var mapping =
-	{
-		"attack": function ()
-		{
+function getFunction(value) {
+	var mapping = {
+		"attack": function () {
 			document.querySelector(".btm1[onclick]").click()
 		},
-		"defend": function ()
-		{
+		"defend": function () {
 			document.querySelector("#ckey_defend").click()
 		},
-		"focus": function ()
-		{
+		"focus": function () {
 			document.querySelector("#ckey_focus").click()
 		},
-		"spirit": function ()
-		{
+		"spirit": function () {
 			document.querySelector("#ckey_spirit").click()
 		},
-		"protection": function ()
-		{
+		"protection": function () {
 			document.querySelector("#qb2").click()
 		},
-		"heal": function ()
-		{
+		"heal": function () {
 			document.querySelector("#qb1").click()
 		},
-		"fire": function ()
-		{
+		"fire": function () {
 			document.querySelector("#qb4").click()
-			setTimeout(function ()
-			{
+			setTimeout(function () {
 				var n = groupedMonsters();
 				console.log("targeting ", n);
 				document.querySelectorAll("#pane_monster > div")[n].click();
@@ -126,24 +103,21 @@ function getFunction(value)
 		}
 	};
 
-	if (!mapping[value])
-	{
+	if (!mapping[value]) {
 		return
 	}
 
-	return mapping[value] == null ? mapping["attack"] : mapping[value];
+	return mapping[value] == null ? mapping.attack : mapping[value];
 }
 
 var interval;
 
-function start()
-{
+function start() {
 	interval = loop();
 	this.onclick = stop;
 	this.textContent = "stop";
 }
-function stop()
-{
+function stop() {
 	clearInterval(interval);
 	this.onclick = start;
 	this.textContent = "start";
